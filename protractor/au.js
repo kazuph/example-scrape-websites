80ab89fa99506ccfa4bc54338624de37c8c7844f
Kazuhiro Homma <kazu.homma@gmail.com>
2015-01-05 23:49:41 +0900

    AUの通信量取得スクリプトを簡素化

--- protractor/au.js
+++ protractor/au.js
@@ -1,17 +1,6 @@
-var fs = require('fs');
-
-var lastMonth = new Date();
-    lastMonth.setMonth(lastMonth.getMonth() - 1);
-
-var outFilePath = [
-  'au-', lastMonth.getFullYear(), '-', (lastMonth.getMonth() + 101).toString().substr(-2), '.tsv'
-].join('');
-
 function $x (xpath) {
   return element(by.xpath(xpath));
 }
-
-var content;
 
 describe('au', function () {
   browser.ignoreSynchronization = true;
@@ -30,19 +19,11 @@ describe('au', function () {
   it('LTE通信量合計を取得する', function () {
     function readInformationIfAny() {
       browser.getCurrentUrl().then(function (url) {
-        var amount = $x('//*[@id="innerBox"]/div[6]/div[2]/div[2]');
-
+        var amount = element(by.xpath('//*[@id="innerBox"]/div[6]/div[2]/div[2]'));
         amount.getText().then(console.log);
-        amount.getText().then(function (a) {
-          content = a
-        });
       });
     }
     readInformationIfAny();
   });
 
-  it('ファイルに書き出す', function () {
-    fs.writeFileSync(outFilePath, content);
-    console.log('wrote: ' + outFilePath);
-  });
 });
